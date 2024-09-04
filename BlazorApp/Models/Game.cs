66d0a52058aceea99099692ae384df0549951455
MalusDark <mcdabestkot@yandex.ru>
2024-09-04 14:09:02 +0300

    Вывод каталога карточек

---
+++ BlazorApp/Models/Game.cs
@@ -0,0 +1,20 @@
+﻿namespace BlazorApp.Models
+{
+    public class Game
+    {
+        private int _id;
+        private string _name;
+        private string _genre;
+
+        public Game(int id, string name, string description)
+        {
+            this._id = id;
+            this._name = name; 
+            this._genre = description;
+        }
+
+        public int Id { get => _id; set => _id = value; }
+        public string Name { get => _name; set => _name = value; }
+        public string Genre { get => _genre; set => _genre = value; }
+    }
+}
---
+++ BlazorApp/Models/Games.cs
@@ -0,0 +1,21 @@
+﻿namespace BlazorApp.Models
+{
+    public class Games
+    {
+        private List<Game> _games;
+
+        public Games() { 
+        _games = new List<Game>();
+            _games.Add(new Game(1, "Escape from Tarkov", "Shooter"));
+            _games.Add(new Game(2, "Minecraft", "Sandbox"));
+        }
+        public void Add(Game game)
+        {
+            _games.Add(game);
+        }
+        public List<Game> GetGames()
+        {
+            return _games;
+        }
+    }
+}
